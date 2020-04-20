440c03ad5a091058f44154247b99030264c59736
Grisilda <46270641+Grisilda@users.noreply.github.com>
2020-04-20 13:26:44 +0200

    Add files via upload

---
+++ user/email/php/restorAccount.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+	if(isset($_POST["submit"])){
+		$connect=mysqli_connect('localhost','id12990860_kizlar','grisilda123','id12990860_fitfydb') or die('Couldnt connect');
+		if($_POST["ussername"]!=null && $_POST["password"]!=null){
+			// echo "Here";
+				$name=$_POST["ussername"];
+				$password=$_POST["password"];
+				$hash = md5($password);
+				$query="Select * from users where username="."'$name'";
+
+				$data=mysqli_query($connect,$query);
+				if(mysqli_num_rows ($data)!=0){
+					$query="Update users set password="."'$hash'"." where username="."'$name'";
+					$data=mysqli_query($connect,$query);
+					header("Location: ../../logimi/html/logimi.html");
+				}else{
+					header('Location:../html/logimi.html');
+				}
+		}else{
+			echo "<script type='text/javascript'>alert('Please fill all the fields!');
+					window.location.href='../html/logimi.html';
+				  </script>";
+		}
+	}
+?>
