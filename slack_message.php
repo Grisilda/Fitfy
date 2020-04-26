34d321cbc1ce1be7248f96f9b74a5f7d731a003b
Grisilda <46270641+Grisilda@users.noreply.github.com>
2020-04-26 23:35:58 +0200

    Update slack_message.php

--- slack_message.php
+++ slack_message.php
@@ -4,7 +4,7 @@ function slack($message, $channel)
 // echo "Hereds";die;
     $ch = curl_init("https://slack.com/api/chat.postMessage");
     $data = http_build_query([
-        "token" => "xoxp-1017216968501-1017216968597-1019428548278-83c4e90012bd0e83a5b65dcba4bfe58f",
+        "token" => "xxxxxx",
         "channel" => $channel, //"#mychannel",
         "text" => $message, //"Hello, Foo-Bar channel message.",
         "username" => "MySlackBot",
