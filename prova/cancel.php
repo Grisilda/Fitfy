0c730d67ef34c0ccf3b26a7e8b78084c144f0b7a
irinakorreshi <51864951+irinakorreshi@users.noreply.github.com>
2020-05-03 21:00:52 +0200

    Update cancel.php

--- prova/cancel.php
+++ prova/cancel.php
@@ -1,9 +1,11 @@
 <div class="container">
 
 <div class="status">
-<h1 class="error">Your PayPal Transaction has been Canceled</h1>
+	<body style="background-image: url(cancel.jpg);background-repeat: no-repeat;background-attachment: fixed;background-size: 100% 100%;">
+<h1 class="error" style="text-align: center; color: red; margin-top: 150px; " >Your PayPal Transaction has been Canceled</h1>
 
 </div>
 
 
 </div>
+</body>
