c85b03e7c2da0e2f536fe5a313df93d47ba21c8c
Grisilda <46270641+Grisilda@users.noreply.github.com>
2020-05-10 03:55:37 +0200

    Update restorAccount.php

--- coach/email/php/restorAccount.php
+++ coach/email/php/restorAccount.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
 	if(isset($_POST["submit"])){
-		$connect=mysqli_connect('localhost','id12990860_kizlar','grisilda123','id12990860_fitfydb') or die('Couldnt connect');
-		if($_POST["ussername"]!=null && $_POST["password"]!=null){
+		$connect=mysqli_connect('localhost','xxxx','xxxx','xxxx') or die('Couldnt connect');
+		if( $_POST["password"]!=null){
 			// echo "Here";
-				$name=$_POST["ussername"];
+				$name=$_SESSION["username_email"];
 				$password=$_POST["password"];
 				$hash = md5($password);
-				$query="Select * from user where username="."'$name'"."and role=2";
+				$query="Select * from user where username="."'$name'"." and role = 2";
 
 				$data=mysqli_query($connect,$query);
 				if(mysqli_num_rows ($data)!=0){
-					$query="Update user set password="."'$hash'"." where username="."'$name'"."and role=2";
+					$query="Update user set password="."'$hash'"." where username="."'$name'"." and role = 2";
 					$data=mysqli_query($connect,$query);
 					header("Location: ../../logimi/html/logimi.html");
 				}else{
@@ -22,5 +22,6 @@ session_start();
 					window.location.href='../html/logimi.html';
 				  </script>";
 		}
+		$connect -> close();
 	}
 ?>
