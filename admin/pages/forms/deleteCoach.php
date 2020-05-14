a3a26c157f86731aa4630583ec5927314373135c
Ermelinda <47155800+ermelindaaa@users.noreply.github.com>
2020-05-14 14:55:59 +0200

    Update deleteCoach.php

--- admin/pages/forms/deleteCoach.php
+++ admin/pages/forms/deleteCoach.php
@@ -1,5 +1,5 @@
 <?php
-	$connect=mysqli_connect('localhost','id12990860_kizlar','grisilda123','id12990860_fitfydb') or die('Couldnt connect'); 	
+	$connect=mysqli_connect('localhost','xxxx','xxxx','id12990860_fitfydb') or die('Couldnt connect'); 	
 	$id=$_POST['id'];
 	$sqlQuery = "DELETE FROM coach WHERE id = ".$id;
 	// echo $sqlQuery;die;
