b31b1656301332ba6f07a093787566ed79677984
Ermelinda <47155800+ermelindaaa@users.noreply.github.com>
2020-05-15 23:53:12 +0200

    Update deleteCoach.php

--- admin/pages/forms/deleteCoach.php
+++ admin/pages/forms/deleteCoach.php
@@ -1,7 +1,7 @@
 <?php
 	$connect=mysqli_connect('localhost','xxxx','xxxx','id12990860_fitfydb') or die('Couldnt connect'); 	
 	$id=$_POST['id'];
-	$sqlQuery = "DELETE FROM coach WHERE id = ".$id;
+	$sqlQuery = "DELETE FROM user WHERE id = '".$id."'and role=2";
 	// echo $sqlQuery;die;
 	$result = mysqli_query($connect, $sqlQuery);
 	echo "sucess";
