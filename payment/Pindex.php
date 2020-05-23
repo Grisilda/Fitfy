65e16f6398e6203d9707b1e7cf4dfb2654c1a9f0
Ermelinda <47155800+ermelindaaa@users.noreply.github.com>
2020-05-23 18:29:33 +0200

    Update Pindex.php

--- payment/Pindex.php
+++ payment/Pindex.php
@@ -26,12 +26,12 @@
  </div>
 <center style="margin-top: 300px">   
 <div style="background-color: purple; width: 500px; height: 200px">
-<h1 style="color: orange"> Click Buy Now to continue with payment </h1>
+<h1 style="color: orange"> Click Pay Now to continue with payment </h1>
 <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top" style=" width: 300px; height: 100px;">
-<input type="hidden" name="business" value="<?php echo PAYPAL_ID; ?>">     
+<input type="hidden" name="business" value="<?php echo PAYPAL_ID; ?>"> 
 <input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="LMXGWPR3NMMSQ">
-<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" style="width: 270px; height: 80px">
+<input type="hidden" name="hosted_button_id" value="Y9NZNN6G4SBVS">
+<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" style="width: 270px; height: 80px">
 <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 <div style="background-color: orange ; width:100px; height: :100px;"><a href="../user/register/html/register.html" style="color: black; text-decoration:none"><h3>Back</h3></a></div>
