16c9fa8c018f224c9d27c9aa3d06562ae00e47ad
Grisilda <46270641+Grisilda@users.noreply.github.com>
2020-06-01 01:40:55 +0200

    Update updateSchedule.php

--- admin/pages/forms/updateSchedule.php
+++ admin/pages/forms/updateSchedule.php
@@ -1,7 +1,7 @@
 <?php
 
 	// echo $_POST['name'];die;
-	$connect=mysqli_connect('localhost','id12990860_kizlar','grisilda123','id12990860_fitfydb') or die('Couldnt connect'); 	
+	$connect=mysqli_connect('xxx','xxx','xxx','xxx') or die('Couldnt connect'); 	
 	$id=$_POST['id'];
 	$day=$_POST['day'];
 	$start_time=$_POST['start_time'];
