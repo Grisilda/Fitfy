ffd2e027785f73b9f310393767933c6077c781d5
Grisilda <46270641+Grisilda@users.noreply.github.com>
2020-06-01 01:28:21 +0200

    Update logimi.php

--- coach/logimi/php/logimi.php
+++ coach/logimi/php/logimi.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-		$connect=mysqli_connect('localhost','id12990860_kizlar','grisilda123','id12990860_fitfydb') or die('Couldnt connect');
+		$connect=mysqli_connect('xxx','xxx','xxx','xxx') or die('Couldnt connect');
 		if($_POST["ussername"]!=null && $_POST["password"]!=null){
 			// echo "Here";
 				$name=$_POST["ussername"];
