95e26ae950d2c77f97e1e6b294485433ec77c265
Grisilda <46270641+Grisilda@users.noreply.github.com>
2020-06-01 01:20:56 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -6,8 +6,6 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="PONIGYM Template">
-    <meta name="keywords" content="PONIGYM, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Fitfy Fitness</title>
@@ -31,6 +29,17 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <style>
+    .footercov {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 60%;
+   background-color:transparent;
+   color: white;
+   
+}
+  </style>
 </head>
 
 <body>
@@ -43,23 +52,23 @@
     <header class="header-section header-normal">
         <div class="container">
             <div class="logo">
-                <a href="./index.html">
-                    <img src="img/FitLogo.png" alt="FITFY" width="100px" height="150px">
+                <a href="./index.php">
+                    <img src="img/FitLogo.png" alt="FITFY" width="100px" height="100px">
                 </a>
             </div>
             <div class="nav-menu">
                 <nav class="mainmenu mobile-menu">
-                    <ul>
-                        <li><a href="./index.php">Home</a></li>
-                        <li><a href="./about-us.php">About</a></li>
-                        <li><a href="./schedule.php">Schedule</a></li>
-                        <li class="active"><a href="./gallery.html">Portfolio</a>
-                        <li><a href="./blog.php">Blog</a>
+                       <ul id='ul'>
+                        <li id='Home' class="index" value="index"><a href="./index.php">Kreu</a></li>
+                        <li id='About' class="about-us"><a href="./about-us.php">Rreth Nesh</a></li>
+                        <li id='Schedule' class="schedule"><a href="./schedule.php">Orari</a></li>
+                        <li id='Portfolio' class="active"><a href="./gallery.php">Foto</a></li>
+                       <!-- <li id='Blog' class="blog"><a href="./blog.php">Blog</a>
                             <ul class="dropdown">
-                                <li><a href="blog-details.php">Blog Details</a></li>
+                                <li id='Blog Details' class="blog-details"><a href="blog-details.php">Blog Details</a></li>
                             </ul>
-                        </li>
-                        <li><a href="./contact.php">Contacts</a></li>
+                        </li>-->
+                        <li id='Contacts' class="contact"><a href="./contact.php">Kontaktet</a></li>
                     </ul>
                 </nav>
                 <div class="nav-right search-switch">
@@ -84,10 +93,10 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="site-text">
-                        <h2>Portfolio</h2>
+                        <h2>Foto</h2>
                         <div class="site-breadcrumb">
-                            <a href="./index.php" class="sb-item">Home</a>
-                            <span class="sb-item">Portfolio</span>
+                            <a href="./index.php" class="sb-item">Kreu</a>
+                            <span class="sb-item">Foto</span>
                         </div>
                     </div>
                 </div>
@@ -103,11 +112,11 @@
                 <div class="col-lg-12 text-center">
                     <div class="filter-controls">
                         <ul id="list-group  list-group-horizontal" >
-                            <li class="list-group-item active" data-filter=".all">All Gallery</li>
-                            <li class="list-group-item " data-filter=".fitness">Fitness</li>
-                            <li class="list-group-item " data-filter=".coaching">Coaching</li>
-                            <li class="list-group-item " data-filter=".event">Event</li>
-                            <li class="list-group-item " data-filter=".other">Other</li>
+                            <li class="list-group-item active" data-filter=".all">Te Gjitha</li>
+                            <li class="list-group-item " data-filter=".fitness">Ambjentet e palestrës</li>
+                            <li class="list-group-item " data-filter=".coaching">Stërvitja</li>
+                            <li class="list-group-item " data-filter=".event">Eventet</li>
+                            <li class="list-group-item " data-filter=".other">Të tjera</li>
                         </ul>
                     </div>
                 </div>
@@ -334,53 +343,68 @@
             <div class="row">
                 <div class="col-lg-6">
                     <div class="map-location">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d33245.297803635964!2d-73.76987401620775!3d40.704774398815005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1575866843291!5m2!1sen!2sbd" style="border:0;" allowfullscreen=""></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.5888017284037!2d19.811742915725354!3d41.31780820826798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDE5JzA0LjEiTiAxOcKwNDgnNTAuMSJF!5e0!3m2!1sen!2s!4v1497606026685" style="border:0;" allowfullscreen=""></iframe>
                         <div class="map-widget">
                             <i class="fa fa-map-marker"></i>
                             <div class="map-address">
                                 <img src="img/map-location.jpg" alt="">
                                 <ul class="map-text">
-                                    <li><span>Address:</span> Iris Watson, Box 283, NY</li>
-                                    <li><span>Phone:</span> 12-456-791</li>
+                                    <li><span>Adresa:</span> Qendra Olimpia,pranë Postës Nr.8,Tiranë</li>
+
+                                    <li><span>Numri telefonit:</span>0692563038</li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6">
+
                     <div class="footer-form set-bg" data-setbg="img/contact-form-bg.jpg">
+
                         <div class="row">
+
                             <div class="col-lg-10">
+
                                 <div class="section-title">
-                                    <h2>Request A Call Back</h2>
-                                    <p>Shape your body and burn fat with strength training. With the right equipment
-                                        such as free weights or resistance machines.</p>
+
+                                    <h2>Ne jemi këtu për ju.</h2>
+
+                                    <p>Ju lutemi plotësoni formën e mëposhtme dhe stafi jonë do t'ju vi në ndihmë.</p>
                                 </div>
-                                <form action="#">
+
+                                <form action="slack_message.php" method="post">
                                     <div class="row">
                                         <div class="col-lg-6">
-                                            <input type="text" placeholder="Name">
+                                            <input type="text" placeholder="Name" id="name" name ="name">
                                         </div>
                                         <div class="col-lg-6">
-                                            <input type="text" placeholder="Email">
+                                            <input type="text" placeholder="Email" id="email" name ="email">
                                         </div>
                                         <div class="col-lg-12">
-                                            <input type="text" placeholder="Subject">
-                                            <textarea placeholder="Message"></textarea>
+                                            <input type="text" placeholder="Subject" id="subject" name ="subject">
+                                            <textarea placeholder="Message" id="message" name ="message"></textarea>
                                             <button type="submit">Submit <i class="ti-angle-double-right"></i></button>
                                         </div>
                                     </div>
                                 </form>
+
                             </div>
+
                         </div>
+
                     </div>
+
                 </div>
+
             </div>
         </div>
         <div class="register normal-register">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
+                         <div class="footercov">
+                       <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public" target="_blank"> <img src="home.svg" style="height:50px;"></a>
+                    </div>
                        <!-- <div class="copyright">
                             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 <!--Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
@@ -409,8 +433,18 @@
     <div class="search-model">
         <div class="h-100 d-flex align-items-center justify-content-center">
             <div class="search-close-switch">+</div>
-            <form class="search-model-form">
-                <input type="text" id="search-input" placeholder="Search here.....">
+            <!-- <form id="searchForm" action="javascript:search();"> -->
+            <form class="search-model-form" id="searchForm" action="javascript:search();">
+               <div class="input-group">
+                  <button id="go" class="btn btn-default" type="button" 
+                          onclick="document.getElementById('searchForm').submit(); return false;">
+                  </button>
+                  <!-- <input type="text" id="searchItem" class="form-control" placeholder="Search here ..."> -->
+                  <input type="text" id="searchItem" placeholder="Search here.....">
+               </div>
+               <ul id="result" style="padding-left: 64px;;margin: 17px;">
+                        
+                    </ul>
             </form>
         </div>
     </div>
@@ -422,6 +456,29 @@ $(document).ready(function() {
         $(this).addClass("active"); 
       }); 
     }); 
+  function search() {
+        debugger;
+       var name = document.getElementById("searchForm").elements["searchItem"].value;
+       var pattern = name.toLowerCase();
+       var targetId = "";
+     
+       var divs = document.getElementsByClassName("nav-menu");
+       for (var i = 0; i < divs.length; i++) {
+          var para = divs[i].getElementsByTagName("li");
+          for (var j = 0; j < para.length; j++) {
+             var index = para[j].innerText.toLowerCase().indexOf(pattern);
+              if (index != -1) {
+                 targetId = para[j].id;
+                 var clasa=para[j].className;
+                 $("#result").empty();
+                 // $("#result").append(para[j].id);
+                 $("#result").append('<a href="https://fitfy.000webhostapp.com/'+clasa+'.php">'+targetId+'</a>');
+                 // $("#ul").append(para[j]);
+                 break;
+              }
+          }
+       }  
+    }
 </script>
     <!-- Js Plugins -->
     <script src="js/jquery-3.3.1.min.js"></script>
