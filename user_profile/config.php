5f2185e0702c6307d03884ba0a7194be988822c8
Ermelinda <47155800+ermelindaaa@users.noreply.github.com>
2020-06-01 03:32:00 +0200

    Add files via upload

---
+++ user_profile/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+ // Database configuration 
+
+ define('DB_HOST', 'localhost'); 
+
+ define('DB_USERNAME', 'id12990860_kizlar'); 
+ define('DB_PASSWORD', 'grisilda123'); 
+
+ define('DB_NAME', 'id12990860_fitfydb'); 
+
+?>
